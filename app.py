ab38840c425e2da9d781fba90d48156cfb0cdfda
geotrieu <trieugeorge@gmail.com>
2020-11-21 21:29:51 -0500

    experimental code to implement model

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
-clf_model = pickle.load(open('model.pkl', 'rb'))
+clf_model = pickle.load(open('beta_model_1.pkl', 'rb'))
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    a = [[9461, 50000, 637, 406597.5, 202500.0]]
+    pred = clf_model.predict(a)
+    return render_template("index.html", data=pred)
 
 
 if __name__ == "__main__":
@@ -17,7 +19,7 @@ if __name__ == "__main__":
 
 @app.route('/submit', methods=['POST'])
 def run():
-    a = []
+    a = [[9461, 50000, 637, 406597.5, 202500.0]]
     for i in range(6):
         a[i] = request.form["attribute" + str(i)]
         if a[i] is None and i < 5:
