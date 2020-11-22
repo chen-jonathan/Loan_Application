c88be1fdda715385b55d2fb20a6ae00aa14b6a75
geotrieu <trieugeorge@gmail.com>
2020-11-22 01:44:55 -0500

    Added reasons

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ clf_model = pickle.load(open('beta_model_3.pkl', 'rb'))
 
 @app.route("/")
 def home():
-    a = [[0, 0, 0, 0, 0, 3000, 0, 66, 360, 0, 1]]
+    a = [[0, 0, 0, 0, 0, 3000, 0, 66, 360, 0, 0]]
     pred = clf_model.predict(a)
     print(pred)
+    reasons = None
     if pred == 'N':
         reasons = getFactors(a)
-    return render_template("index.html", data=pred)
+    print(reasons)
+    return render_template("index.html", data=pred, reasons=reasons, len=len(reasons))
 
 
 if __name__ == "__main__":
@@ -35,4 +37,6 @@ def getFactors(a):
     factors = []
     if a[9] == 0: # Bad Credit History
         factors.append("Your Credit History is insufficient. Try getting more credit experience before applying again.")
-
+    if a[10] == 0: # Rural Customer
+        factors.append("Rural Customers have a more difficult time getting a loan based on property appreciation. Try improving other factors before applying again.")
+    return factors
