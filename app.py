bae007c2bf454a0bef363b2ba826a52b9c483cc7
Jonathan <jc09310@gmail.com>
2020-11-21 20:39:32 -0500

    Renamed attributes

--- app.py
+++ app.py
@@ -5,20 +5,23 @@ import numpy as np
 app = Flask(__name__)
 clf_model = pickle.load(open('model.pkl', 'rb'))
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 if __name__ == "__main__":
     app.run()
 
+
 @app.route('/submit', methods=['POST'])
 def run():
-        a = []
-        for i in range(6):
-            a[i] = request.form["attribute" + str(i)]
-            if(a[i] == null && i < 5):
-                a[i] = 0
-        arr = np.array(a)
-        pred = clf_model.predict(arr)
-        return render_template("index.html", data=pred)
+    a = []
+    for i in range(6):
+        a[i] = request.form["attribute" + str(i)]
+        if a[i] is None and i < 5:
+            a[i] = 0
+    arr = np.array(a)
+    pred = clf_model.predict(arr)
+    return render_template("index.html", data=pred)
